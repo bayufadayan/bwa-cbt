6ab41055d5d6ee8b89ef691930a4b0cc5404e425
bayufadayan <bayufadayan@gmail.com>
2025-08-20 12:38:08 +0700

    feat: Manage course (edit and update question)

--- app/Http/Controllers/CourseQuestionController.php
+++ app/Http/Controllers/CourseQuestionController.php
@@ -62,7 +62,6 @@ class CourseQuestionController extends Controller
             DB::commit();
 
             return redirect(route('courses.show', $course->id));
-
         } catch (\Exception $e) {
             DB::rollBack();
             $err = ValidationException::withMessages([
@@ -86,7 +85,13 @@ class CourseQuestionController extends Controller
      */
     public function edit(CourseQuestion $courseQuestion)
     {
-        //
+        $course = $courseQuestion->course;
+        $students = $course->users()->orderBy('id', 'DESC')->get();
+        return view('admin.questions.edit', [
+            "course" => $course,
+            "courseQuestion" => $courseQuestion,
+            "students" => $students,
+        ]);
     }
 
     /**
@@ -94,7 +99,42 @@ class CourseQuestionController extends Controller
      */
     public function update(Request $request, CourseQuestion $courseQuestion)
     {
-        //
+        $validated = $request->validate([
+            'question' => 'required|string|max:255',
+            'answers' => 'required|array',
+            'answers.*' => 'required|string|max:255',
+            'correct_answer' => 'required|integer',
+        ]);
+
+        DB::beginTransaction();
+
+        try {
+            $courseQuestion->update([
+                'question' => $request->question,
+            ]);
+
+            // Hapus data jawaban
+            $courseQuestion->course_answers()->delete();
+            foreach ($request->answers as $index => $answerText) {
+                $isCorrect = $request->correct_answer == $index;
+
+                $courseQuestion->course_answers()->create([
+                    'answer' => $answerText,
+                    'is_corrent' => $isCorrect,
+                ]);
+            }
+
+            DB::commit();
+
+            return redirect(route('courses.show', $courseQuestion->course_id));
+        } catch (\Exception $e) {
+            DB::rollBack();
+            $err = ValidationException::withMessages([
+                'system_error' => ['System Error!' . $e->getMessage()],
+            ]);
+
+            throw $err;
+        }
     }
 
     /**
